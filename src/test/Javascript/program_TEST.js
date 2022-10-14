c1db9d2d4f518c69f1c1ea84b8977f2d339dd027
Christiana Ramey <christianaramey@gmail.com>
2022-10-14 15:47:21 -0400

    Karma Config

--- src/test/Javascript/program_TEST.js
+++ src/test/Javascript/program_TEST.js
@@ -18,6 +18,10 @@ QUnit.test('input = 7', assert => {
   assert.equal(factorial(7), 5);
 });
 
+QUnit.test('input = 8', assert => {
+  assert.equal(factorial(8), 21540);
+});
+
 QUnit.test('input = -100', assert => {
   assert.equal(factorial(-100), -1);
 });
